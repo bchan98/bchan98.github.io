a47b82646b4ada64fcc2e077e1d7242b3aea0e5e
Brendan Chan <brendan.mk.chan@gmail.com>
2024-08-22 21:33:20 -0700

    Added commenting for basic structure idea

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,21 @@ export default function Home() {
         <div>
             Hello world!
             <div id="landing-page"></div>
-            <div id="experience"></div>
+            {
+                // landing image with animation, my name is Brendan Chan, a software engineer, provide link to download cv/contact me which drags to bottom
+            }
+            <div id="qualifications"></div>
+            {
+                // work experience - dataannotation tech, skills with typescript, javascript, python etc 
+            }
             <div id="portfolio"></div>
+            {
+                // show 3 projects - ballersim.app, links to appointmentproject, inventory manager, todolist/deliverysim?
+            }
             <div id="contact"></div>
+            {
+                // show box for contact me, include link to linkedin/github
+            }
         </div>
     );
 }
