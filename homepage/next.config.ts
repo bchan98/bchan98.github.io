18c7c616dab06cdebcdbf5b031f9af32e4ead776
Brendan Chan <brendan.mk.chan@gmail.com>
2025-05-15 22:26:10 -0700

    Added gh deploy
    Updated so gh pages will work properly

--- homepage/next.config.ts
+++ homepage/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = proccess.env.GITHUB_PAGES === 'true';
+const repo = 'bchan98.github.io';
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+    basepath: isGithubPages ? `/${repo}` : '',
+    assetPrefix: isGithubPages ? `/${repo}` : '',
 };
+
 
 export default nextConfig;
